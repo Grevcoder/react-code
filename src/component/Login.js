9581bf42a6bce28567c262c06819fcad0cddb542
Autoseleproject <ev.greeshma@gmail.com>
2022-04-03 22:36:53 -0500

    login

--- src/component/Login.js
+++ src/component/Login.js
@@ -3,13 +3,7 @@ import { BrowserRouter,Link,Route, Routes, useNavigate } from "react-router-dom"
 import React from "react";
 import Fail from "./Fail";
 import Sucess from "./Sucess";
-
- /*const components={
-        userSucess:UserSucess
-    }*/
-
-function Login() {
-   
+function Login() {  
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
@@ -17,7 +11,7 @@ function Login() {
   //const selectUser=components[props.user];
   function handleChange() {
     //validation
-    if (userName == "Greeshma" && password=="password" && email=="greeshma@mail.com") {
+    if (userName === "Greeshma" && password==="password" && email==="greeshma@mail.com") {
       //navigate to another page
      navigate('/Sucess')
     } else {
